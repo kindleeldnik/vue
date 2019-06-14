db85324ae5ccb0069cbee2e06f6f0afee8a6a09a
kindleeldnik <37297312@qq.com>
2019-06-14 12:57:16 +0800

    add createComponents

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './routers'
+
+import store from './stores'
+
+import axios from 'axios'
+Vue.prototype.axios = axios
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
